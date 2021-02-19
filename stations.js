3e771fe8c8bb5e413abc191a091206ca81df3dd9
bosnicradovan <bosnicradovan@gmail.com>
2021-02-19 17:52:32 +0000

    <stations>

---
+++ stations.js
@@ -0,0 +1,44 @@
+const chooseStations = function (stations) {
+   stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+  const goodStations = [];
+  for (station of stations) {
+    const capacity = station[1]
+    if (capacity >= 20) {
+      const type = station[2]
+      if (type === 'school' || type === 'community centre') {
+        goodStations.push(station[0])
+    }
+  }
+  return goodStations
+}
+
+console.log(chooseStations(stations));
+}
+
+
+const chooseStations = function (stations) {
+  stations = [
+ ['Big Bear Donair', 10, 'restaurant'],
+ ['Bright Lights Elementary', 50, 'school'],
+ ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+ const goodStations = [];
+ for (const station of stations) {
+   
+   const  capacity = station[1]
+   
+   if (capacity >= 20) {
+     const type = station[2]
+     if (type === "community centre" || type === "school") {
+     goodStations.push(station[0])
+   }
+   }
+ }
+ return goodStations
+}
