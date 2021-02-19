1d733a5fd1d73d113fd508dd1dfb33bf060f5f54
bosnicradovan <77697477+bosnicradovan@users.noreply.github.com>
2021-02-19 13:58:36 -0800

    Update stations.js

--- stations.js
+++ stations.js
@@ -1,19 +1,19 @@
 const chooseStations = function (stations) {
-   stations = [
-  ['Big Bear Donair', 10, 'restaurant'],
-  ['Bright Lights Elementary', 50, 'school'],
-  ['Moose Mountain Community Centre', 45, 'community centre']
-];
+  // ['Big Bear Donair', 10, 'restaurant'],
+  // ['Bright Lights Elementary', 50, 'school'],
+  // ['Moose Mountain Community Centre', 45, 'community centre']
 
-  const goodStations = [];
-  for (station of stations) {
-    const capacity = station[1]
+  let goodStations = [];
+  for (let station of stations) {
+    const  capacity = station[1]
+    const type = station[2]
     if (capacity >= 20) {
-      const type = station[2]
-      if (type === 'school' || type === 'community centre') {
+      if (type === "community centre" || type === "school") {
         goodStations.push(station[0])
+      }
     }
   }
-  return goodStations
+  return goodStations;
+
 }
 
