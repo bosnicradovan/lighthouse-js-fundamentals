9066d5b2128ec3d55dc6b739ee02a0e8cacbfc88
bosnicradovan <bosnicradovan@gmail.com>
2021-03-05 00:52:09 +0000

    <LHL exercise>

---
+++ MergeArrays.js
@@ -0,0 +1,7 @@
+const merge = function(array, array2) {
+  return array.concat(array2).sort();
+};
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
